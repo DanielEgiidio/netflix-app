87ee0ee09df4d6e79f8971f774319da797e9bed9
DanielEgidiio <danielegidioti@gmail.com>
2021-12-23 17:25:11 -0300

    Criação do banner e estilização

--- src/services/Requests.js
+++ src/services/Requests.js
@@ -3,13 +3,13 @@ import apikey from "./apikey";
 
 const requests = {
     fetchTreding: `/trending/all/week?api_key=${apikey}&language=pt-BR`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${apikey}&with_network=213`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${apikey}&with_network=213&language=pt-BR`,
     fetchTopRated: `/movie/top_rated?api_key=${apikey}&language=pt-BR`,
-    fetchActionMovies: `/discover/movie?api_key=${apikey}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${apikey}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${apikey}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${apikey}&with_genres=10749`,
-    fetchDocumentaryMovies: `/discover/movie?api_key=${apikey}&with_genres=99`,
+    fetchActionMovies: `/discover/movie?api_key=${apikey}&with_genres=28&language=pt-BR`,
+    fetchComedyMovies: `/discover/movie?api_key=${apikey}&with_genres=35&language=pt-BR`,
+    fetchHorrorMovies: `/discover/movie?api_key=${apikey}&with_genres=27&language=pt-BR`,
+    fetchRomanceMovies: `/discover/movie?api_key=${apikey}&with_genres=10749&language=pt-BR`,
+    fetchDocumentaryMovies: `/discover/movie?api_key=${apikey}&with_genres=99&language=pt-BR`,
 }
 
 export default requests;
